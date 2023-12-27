7f39ce9ec8f16bcfbe1dbad58e1f43b666d994af
Ahmed <ahmed.m112000@hotmail.com>
2023-12-27 16:09:41 +0400

    feat: add skill and opinion to dom

--- src/script.js
+++ src/script.js
@@ -5,6 +5,35 @@ function addSkill() {
   if (skill) {
     console.log(skill, opinion);
 
+    // format
+    /* 
+    <div
+      class="flex justify-between border-8 border-t-0 border-solid border-green-400 p-4"
+    >
+      <div>
+        <p class="text-3xl">This is 40 chars input for demo purposes</p>
+      </div>
+      <p class="">😀</p>
+    </div>
+    */
+    const parentDiv = document.createElement("div");
+    const innerDiv = document.createElement("div");
+
+    const pSkill = document.createElement("p");
+    pSkill.innerHTML = skill;
+
+    // TODO: make a function to return the correct emoji based on opinion
+    const pOpinion = document.createElement("p");
+    pOpinion.innerHTML = opinion;
+
+    innerDiv.appendChild(pSkill);
+    parentDiv.appendChild(innerDiv);
+    parentDiv.appendChild(pOpinion);
+
+    const main = document.querySelector("main");
+    main.appendChild(parentDiv);
+    console.log(main);
+
     // reset values
     document.querySelector(".skill").value = "";
     document.querySelector(".opinion").value = "confident";
