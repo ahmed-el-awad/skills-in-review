c3c7714686eceae19d3ffc08fbd9232e10ad2d2d
Ahmed <ahmed.m112000@hotmail.com>
2024-01-18 12:28:21 +0400

    refactor: change file structure for easier deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/*.html"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+  content: ["./public/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
